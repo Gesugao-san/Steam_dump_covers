3645348ef4cff1d4312c514ff715f0503061f7bf
Gesugao-san <gesugao.san@gmail.com>
2023-03-21 11:38:48 +0300

    Add presence_db.json

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 
 
+from datetime import datetime
+from steam.steamid import SteamID
 from urllib.error import URLError, HTTPError
 import json
 import os
+import signal
+import sys
 import urllib.request
 import vdf
-from steam.steamid import SteamID
-from datetime import datetime
 
 
 base_url = "https://steamcdn-a.akamaihd.net/steam/apps/"
@@ -25,12 +27,29 @@ base_urls = {
 
 print("Hi")
 
+def save_presence_db(_presence_data):
+	print("Saving \"data/presence_db.json\" file... ", end='')
+	with open("data/presence_db.json", "w", encoding='utf-8') as presence_db_file:
+		presence_db_file.write(json.dumps(_presence_data, indent=2) + "\n")
+		presence_db_file.close()
+	print("done.")
+
+if not os.path.isfile("data/presence_db.json"):
+	print("No \"data/presence_db.json\" file, creating...")
+	save_presence_db({})
+	presence_data_persistent = {}
+else:
+	with open("data/presence_db.json", "r", encoding='utf-8') as presence_db_file:
+		presence_data_persistent = json.load(presence_db_file)
+		presence_db_file.close()
+
+
 if not os.path.isfile("data/settings.json"):
 	raise SystemExit("no settings file")
-
-with open("data/settings.json", "r", encoding='utf-8') as settings_data_f:
-	settings = json.load(settings_data_f)
-	#settings_data_f.close()
+else:
+	with open("data/settings.json", "r", encoding='utf-8') as settings_data_f:
+		settings = json.load(settings_data_f)
+		#settings_data_f.close()
 
 if not "path_steam_core" in settings:
 	raise SystemExit("no \"path_steam_core\" param in settings file")
@@ -85,7 +104,7 @@ with open("data/input.json", "r", encoding='utf-8') as input_data_f:
 #print(type(input_data))
 
 
-#print("App ID:", appid)
+#print("App ID:", appid_str)
 longest_gameid = len(str(max(input_data))) #7 # 2290190, for example
 longest_base_url = 0
 just_len = 0  # justify
@@ -96,22 +115,45 @@ for key, value in base_urls.items():
 	if just_len < check_len:
 		just_len = check_len
 just_len -= 2
-print('justify len:', just_len)
+max_missing_data = len(base_urls.keys())
+print('justify len:', just_len, "max_missing_data:", max_missing_data)
+
+# https://stackoverflow.com/a/1112350/8175291
+def signal_handler(sig, frame):
+	print("\n")
+	print("presence_data_persistent:", presence_data_persistent)
+	print('You pressed Ctrl-C!')
+	#sys.exit(0)
+	save_presence_db(presence_data_persistent)
+	exit()
+
+signal.signal(signal.SIGINT, signal_handler)
+#print('Press Ctrl+C')
+#signal.pause()
 
 
-for appid in input_data:
-	for key, value in base_urls.items():
-		tmp_url = base_url + str(appid) + "/" + value
-		tmp_name = str(appid) + "_" + value
+for appid_int in input_data:
+	appid_str = str(appid_int)
+	presence_data_persistent = presence_data_persistent
+	presence_data_temp = {appid_str: {}}
+	presence_missing_count = 0
+	for surl_name, surl in base_urls.items():
+		url_index = list(base_urls.values()).index(surl) + 1 #surl_name
+		tmp_url = base_url + appid_str + "/" + surl
+		tmp_name = appid_str + "_" + surl
 		tmp_file = settings["path_output"] + "\\" + tmp_name
 		tmp_exist = os.path.isfile(tmp_file)
-		tmp_status1 = str(appid).ljust(longest_gameid + 1) + value.split('.')[0].ljust(longest_base_url - 3)  #'"' + tmp_name + '":'
+		tmp_status1 = appid_str.ljust(longest_gameid + 1) + surl.split('.')[0].ljust(longest_base_url - 3)  #'"' + tmp_name + '":'
 		print(tmp_status1.ljust(just_len), end='')
 		#print(key, '->', value)
 		if tmp_exist:
 			tmp_status2 = "OK"
-			tmp_status3 = "409  Already found" #exists
-			print(tmp_status2.ljust(4), tmp_status3)
+			tmp_status3 = "409 Already found" #exists
+			if presence_data_persistent.get(appid_str) != None:
+				presence_data_persistent[appid_str].update({url_index: True})
+			else:
+				presence_data_persistent[appid_str] = {url_index: True}
+			print(tmp_status2.ljust(3), tmp_status3)
 		else:
 			#tmp_status2 = "↓… " #"DOWNLOAD… " #"💾" #"Downloading... "
 			#print("File not exist, start downloading...")
@@ -121,29 +163,53 @@ for appid in input_data:
 					f_out = f_web.read() #.decode('utf-8')
 					with open(tmp_file, "wb") as fcont:
 						fcont.write(f_out)
-					tmp_status2 = "OK" #"Downloaded successfully"
+					tmp_status2 = "OK 200" #"Downloaded successfully"
 					print(tmp_status2)
+					if presence_data_persistent.get(appid_str) != None:
+						presence_data_persistent[appid_str].update({url_index: True})
+					else:
+						presence_data_persistent[appid_str] = {url_index: True}
+					save_presence_db(presence_data_persistent)
 			except HTTPError as e:
 				# "e" can be treated as a http.client.HTTPResponse object
 				tmp_status2 = "ERR"
 				if e.code == 404:
-					tmp_status3 = str(e.code) + "  " + str(e.reason) # remotely # on Steam
+					tmp_status3 = str(e.code) + " " + str(e.reason) # remotely # on Steam
+					if presence_data_persistent.get(appid_str) != None:
+						presence_data_persistent[appid_str].update({url_index: e.code})
+					else:
+						presence_data_persistent[appid_str] = {url_index: e.code}
+					presence_missing_count += 1
 				elif e.code == 429:
-					tmp_status3 = str(e.code) + "  " + str(e.reason)
-					print(tmp_status2.ljust(4), tmp_status3)
+					tmp_status3 = str(e.code) + " " + str(e.reason)
+					if presence_data_persistent.get(appid_str) != None:
+						presence_data_persistent[appid_str].update({url_index: e.code})
+					else:
+						presence_data_persistent[appid_str] = {url_index: e.code}
+					print(tmp_status2.ljust(3), tmp_status3)
 					print(str(datetime.now()) + " Stoped bombing Steam.")
+					presence_missing_count += 1
+
+					save_presence_db(presence_data_persistent)
 					exit()
 				else:
+					presence_missing_count += 1
 					tmp_status3 = "HTTPError:" + str(e.code)
-				print(tmp_status2.ljust(4), tmp_status3)
+				print(tmp_status2.ljust(3), tmp_status3)
 			except URLError as e:
 				# https://stackoverflow.com/a/29538780/8175291
 				tmp_status2 = "ERR"
 				tmp_status3 = "URLError:" + str(e.reason)
-				print(tmp_status2.ljust(4), tmp_status3)
+				print(tmp_status2.ljust(3), tmp_status3)
+				if presence_data_persistent.get(appid_str) != None:
+					presence_data_persistent[appid_str].update({url_index: str(e)})
+				else:
+					presence_data_persistent[appid_str] = {url_index: str(e)}
+				presence_missing_count += 1
 			#else:
 			#	tmp_status2 = "Downloaded successfully"
 			#	print("File \"" + tmp_name + "\" -", tmp_status2)
+	save_presence_db(presence_data_persistent)
 	#raise SystemExit("ToDo")
 
 
